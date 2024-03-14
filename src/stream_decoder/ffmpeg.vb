fe156d48cf6be7a3980be63c9ec3d1647619dd99
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-03-14 19:56:53 +0800

    fix for the input of ffmpeg

--- src/stream_decoder/ffmpeg.vb
+++ src/stream_decoder/ffmpeg.vb
@@ -30,7 +30,8 @@ Module ffmpeg
     Public Function Combine(video As String, audio As String, save_mp4 As String) As Integer
         Dim code As Integer = 0
         ' ffmpeg -i video.mp4 -i audio.aac -c:v copy -strict experimental save.mp4
-        Dim print As String = PipelineProcess.Call(ffmpeg, $"-i {video.CLIPath} -i {audio.CLIPath} -c:v copy -strict experimental {save_mp4.CLIPath}", exitCode:=code)
+        Dim args As String = $"-i {video.CLIPath} -i {audio.CLIPath} -c:v copy -strict experimental {save_mp4.CLIPath}"
+        Dim print As String = PipelineProcess.Call(ffmpeg, args, [in]:="y" & vbCrLf & "y" & vbCrLf & "y", exitCode:=code)
         Dim logfile As String = save_mp4.ChangeSuffix("log")
 
         Call print.SaveTo(logfile)
