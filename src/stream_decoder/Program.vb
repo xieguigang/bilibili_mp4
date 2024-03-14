7f14ab0845cebf25cbe33d153eedd02bc415af61
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-03-14 19:13:45 +0800

    get files input and outputs

--- src/stream_decoder/Program.vb
+++ src/stream_decoder/Program.vb
@@ -10,7 +10,10 @@ Module Program
     End Function
 
     <ExportAPI("/convert")>
+    <Usage("/convert /src <folder_path> [/save <output.mp4>]")>
     Public Function Convert(args As CommandLine) As Integer
+        Dim src As String = args("/src")
+        Dim save As String = args("/save") Or $"{src}/output.mp4"
 
     End Function
 End Module
