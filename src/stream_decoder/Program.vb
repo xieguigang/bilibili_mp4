79e6e4e3507dfa70fd02e919f17f74416453ee2a
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-03-14 19:20:41 +0800

    comvert workflow

--- src/stream_decoder/Program.vb
+++ src/stream_decoder/Program.vb
@@ -1,3 +1,4 @@
+Imports System.IO
 Imports Microsoft.VisualBasic.CommandLine
 Imports Microsoft.VisualBasic.CommandLine.InteropService.SharedORM
 Imports Microsoft.VisualBasic.CommandLine.Reflection
@@ -14,6 +15,27 @@ Module Program
     Public Function Convert(args As CommandLine) As Integer
         Dim src As String = args("/src")
         Dim save As String = args("/save") Or $"{src}/output.mp4"
+        Dim m4s As String() = src.ListFiles("*.m4s").ToArray
+        Dim video As String = m4s.Where(Function(s) s.BaseName.EndsWith("30120.m4s")).First
+        Dim audio As String = m4s.Where(Function(s) s.BaseName.EndsWith("30280.m4s")).First
+        Dim video_mp4 As String = video.ChangeSuffix("mp4")
+        Dim audio_aac As String = audio.ChangeSuffix("aac")
 
+        Call stream_offset(video, video_mp4)
+        Call stream_offset(audio, audio_aac)
+
+        ' call ffmpeg commandline 
+        Return ffmpeg.Combine(video_mp4, audio_aac, save_mp4:=save)
     End Function
+
+    Private Sub stream_offset(filepath As String, save As String)
+        Dim s As Stream = filepath.Open(FileMode.Open, doClear:=False, [readOnly]:=True)
+        Dim out As Stream = save.Open(FileMode.OpenOrCreate, doClear:=True, [readOnly]:=False)
+
+        Call s.Seek(9, SeekOrigin.Begin)
+        Call s.CopyTo(out)
+        Call out.Flush()
+        Call out.Close()
+        Call s.Close()
+    End Sub
 End Module
--- src/stream_decoder/ffmpeg.vb
+++ src/stream_decoder/ffmpeg.vb
@@ -1,3 +1,6 @@
 ﻿Module ffmpeg
 
+    Public Function Combine(video As String, audio As String, save_mp4 As String) As Integer
+
+    End Function
 End Module
