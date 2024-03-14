a5f5e5cea5957be4a949ddb9b8b5bd6bb3846695
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-03-14 19:28:10 +0800

    add ffmpeg exe file

--- src/stream_decoder/ffmpeg.vb
+++ src/stream_decoder/ffmpeg.vb
@@ -1,6 +1,8 @@
 ﻿Module ffmpeg
 
     Public Function Combine(video As String, audio As String, save_mp4 As String) As Integer
+        Dim ffmpeg As String = $"{App.HOME}/"
 
+        ' ffmpeg -i video.mp4 -i audio.aac -c:v copy -strict experimental save.mp4
     End Function
 End Module
